6efa77ccfbcb0602b669690e254295b60de1eeef
surendra <134360600+surendraveeravalli@users.noreply.github.com>
2023-05-27 19:26:56 +0530

    practice

---
+++ homework2705.js
@@ -0,0 +1,93 @@
+for (i=1;i<=10;i++){
+    console.log(i);
+}
+
+for(i=20; i>=10;i--){
+    console.log(i);
+}
+ for(i=2; i<=14; i+=2){
+    console.log(i);
+ }
+  for(i=1; i<=10; i++){
+    console.log(i+":hello");
+  }
+
+  for(i=50; i>=10; i-=10){
+    console.log(i);
+  }
+
+  var i=34;
+  if(i%2==0){
+    console.log("number is even");
+  }
+else{
+    console.log("number is odd");
+}
+
+var i=-1;
+if(i<0){
+    console.log("negative");
+}
+else{
+    console.log("positive");
+}
+
+var i=32;
+if(i>0){
+    console.log("positive");
+}
+else{
+    console.log("negative");
+}
+
+var age=19;
+if(age>=18){
+    console.log("eligible to vote");
+}
+else{
+    console.log("ineligible");
+}
+
+
+var leapyear=2024;
+if(leapyear%2==0){
+    console.log("leapyear");
+}
+else{
+    console.log("not leapyear");
+}
+
+
+var num=12;
+if(num%2==0 && num%3==0){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+var i=60;
+if(i>=60){
+    console.log("senior citizen");
+}
+else{
+    console.log("not senior citizen");
+}
+
+var array=[1,2,3,4,5,6];
+for(i=0; i<array.length;i++){
+    console.log(array[i]);
+}
+
+var arr=[1,2,3,4,5,6,7,8,9];
+for (i=arr.length-1; i>=0; i--){
+console.log(arr[i]);
+}
+
+
+var aar=[1,2,3,4,5,6,7,8];
+for(i=0;i<=arr.length-1;i++)
+if(i%2==0){
+console.log(i);
+}
+
